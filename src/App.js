42693d84667fb8b64f66038e09806f387c38fcba
john2796 <jbmiranda22796@gmail.com>
2018-12-30 19:23:22 -0800

    abcd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Redux testing</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+          reiciendis libero illo ad alias qui. Ex, ratione. Quod ut numquam
+          necessitatibus perspiciatis. Recusandae culpa, nobis quae laudantium
+          tenetur repudiandae dolore blanditiis nostrum rerum vitae architecto
+          ipsa, quod voluptates libero sit! Sapiente porro aperiam non
+          exercitationem similique assumenda neque incidunt aliquid! Accusantium
+          vitae quisquam tempora excepturi porro, ducimus cum beatae eaque odit
+          ipsum quas magni in veritatis necessitatibus blanditiis! Tenetur
+          assumenda voluptates, hic repellat magni, quibusdam quasi quam
+          voluptatem neque pariatur veniam. Eos mollitia, omnis aperiam tempore
+          facilis rerum adipisci tempora modi dolores nulla ratione ex itaque
+          nemo nobis quia obcaecati exercitationem aut, ipsam quibusdam officiis
+          repellat autem iste doloribus! Dolor rerum, facilis hic eum laudantium
+          fugit impedit nulla nesciunt velit incidunt aperiam iure similique
+          dolorum placeat, accusamus optio debitis consectetur commodi modi
+          sequi earum. Magnam quam sequi tempore voluptatem labore neque commodi
+          accusamus unde quae nobis est voluptates, libero natus, repudiandae
+          quis id aut impedit eaque error totam! Quo ex nisi, ipsum ad iure
+          magnam dolore quia consequuntur incidunt sunt eveniet optio assumenda
+          cum expedita totam commodi maxime dicta voluptatum perspiciatis et
+          blanditiis sequi asperiores dolor. Non aliquid exercitationem beatae
+          animi quos. Totam consequatur quaerat illo sit repellat minus aperiam
+          necessitatibus provident nihil placeat exercitationem architecto
+          explicabo temporibus incidunt numquam ab dignissimos, labore
+          doloremque voluptatum minima. Itaque error asperiores iure quis vitae,
+          minus natus provident, rem quos optio excepturi impedit quam beatae
+          nulla quia eveniet doloribus? Veniam itaque dignissimos nulla porro
+          magni soluta sed cupiditate alias impedit, optio modi necessitatibus
+          dolorem temporibus totam ipsum architecto cumque earum. Praesentium
+          impedit sunt ex, cum itaque obcaecati? Eos, fugiat ut. Fugit quo dolor
+          architecto temporibus nesciunt sequi ipsa optio, ex laudantium maxime
+          odio placeat laborum odit corrupti quasi ut tempore non! Ducimus
+          recusandae tenetur voluptatem veniam nemo quo natus error molestias
+          autem quos!
+        </p>
       </div>
     );
   }
